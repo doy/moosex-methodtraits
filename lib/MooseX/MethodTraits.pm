4c5a069ebf7dd2bd3d5deec55158d56a0b7acf7b
Jesse Luehrs <doy@tozt.net>
2009-09-08 22:25:13 -0500

    initial commit

---
+++ lib/MooseX/MethodTraits.pm
@@ -0,0 +1,71 @@
+package MooseX::MethodTraits;
+use Moose;
+
+=head1 NAME
+
+MooseX::MethodTraits -
+
+=head1 SYNOPSIS
+
+
+=head1 DESCRIPTION
+
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+
+=head1 BUGS
+
+No known bugs.
+
+Please report any bugs through RT: email
+C<bug-moosex-methodtraits at rt.cpan.org>, or browse to
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=MooseX-MethodTraits>.
+
+=head1 SEE ALSO
+
+
+=head1 SUPPORT
+
+You can find this documentation for this module with the perldoc command.
+
+    perldoc MooseX::MethodTraits
+
+You can also look for information at:
+
+=over 4
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/MooseX-MethodTraits>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/MooseX-MethodTraits>
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=MooseX-MethodTraits>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/MooseX-MethodTraits>
+
+=back
+
+=head1 AUTHOR
+
+  Jesse Luehrs <doy at tozt dot net>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2009 by Jesse Luehrs.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as perl itself.
+
+=cut
+
+1;
